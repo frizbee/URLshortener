d2ce6d71cf52a31ebfd57f4f8b0e360f4a4ef591
Andre <mail2andrwa@gmail.com>
2016-07-31 14:14:09 +0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'shorteners/index'
 
-  get 'shorteners/new'
-
-  get 'shorteners/show'
+  resource :shorteners, only: [:index, :new, :show]
+  root "shorteners#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
