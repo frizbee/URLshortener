3168a6e73f021def4f909b1b10e2dc751a9fb8fe
Andre <mail2andrwa@gmail.com>
2016-07-31 14:09:30 +0700

    more tests

--- Gemfile
+++ Gemfile
@@ -32,15 +32,15 @@ gem 'mongoid', git: 'https://github.com/mongodb/mongoid.git'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails','~> 3.5'
+  gem 'rails-controller-testing'
+  gem 'pry-rails'
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
   gem 'listen', '~> 3.0.5'
-  gem 'rspec-rails','~> 3.5'
-  gem 'rails-controller-testing'
-  gem 'pry-rails'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- spec/controllers/shorteners_controller_spec.rb
+++ spec/controllers/shorteners_controller_spec.rb
@@ -5,21 +5,36 @@ RSpec.describe ShortenersController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+
+    it "renders template index" do
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+
+    it "renders template new" do
+      get :new
+      expect(response).to render_template(:new)
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
       get :show
-      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+
+    it "renders template show" do
+      get :show
+      expect(response).to render_template(:show)
     end
   end
 
