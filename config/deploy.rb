90ad0b2afbca188a8ce454bf13cbd24a4186d6c7
Andre <mail2andrwa@gmail.com>
2016-08-03 01:31:36 +0700

    edit deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,10 +12,10 @@ set :rbenv_path, "/home/#{fetch(:user)}/.rbenv/"
 
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/mongoid.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
