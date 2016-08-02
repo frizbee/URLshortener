ce04e9b4725e2da3142c4a7288074f418334545a
AndreAntonov <mail2andrewa@gmail.com>
2016-08-02 10:16:56 +0700

    Update shorteners_controller.rb

--- app/controllers/shorteners_controller.rb
+++ app/controllers/shorteners_controller.rb
@@ -5,6 +5,7 @@ class ShortenersController < ApplicationController
   end
 
   def create
+    # TODO find or initialize by instead of new
     @shortener = Shortener.new(shortener_params)
 
     if @shortener.save
