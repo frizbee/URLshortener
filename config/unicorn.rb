251e1ddffd0b6b0129b7b4c562615a39b4283adf
Andre <mail2andrwa@gmail.com>
2016-08-03 00:35:58 +0700

    added nginx, unicorns configs

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+root = "/home/frizbee/www/shortener/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.shortener.sock"
+worker_processes 2
+timeout 30
+
